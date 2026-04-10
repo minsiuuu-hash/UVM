c48b4ad918bfb1bc9ee1f49b5fa4fb484330ebb3
minsiuuu-hash <01jmsmonster@gmail.com>
2026-04-10 21:03:13 +0900

    Add files via upload

---
+++ APB_RAM/Code/apb_ram_driver.sv
@@ -0,0 +1,72 @@
+`ifndef DRIVER_SV 
+`define DRIVER_SV
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+`include "apb_ram_seq_item.sv"
+
+class apb_driver extends uvm_driver #(apb_seq_item);
+	`uvm_component_utils(apb_driver)
+	virtual apb_if vif;
+
+	function new(string name, uvm_component parent);
+		super.new(name,parent);
+	endfunction
+
+	virtual function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		if (!uvm_config_db#(virtual apb_if)::get(this,"","vif",vif)) begin
+			`uvm_fatal(get_type_name, "can't find vif");
+		end
+	endfunction
+
+	virtual task run_phase(uvm_phase phase);
+		apb_bus_init();
+		wait(vif.presetn == 1);
+		`uvm_info(get_type_name(), " check reset off, wait for transaction..", UVM_MEDIUM)
+
+		forever begin
+			apb_seq_item tx;
+			seq_item_port.get_next_item(tx);
+			drive_apb(tx);
+			seq_item_port.item_done();
+		end
+	endtask
+
+	task apb_bus_init();
+		vif.drv_cb.psel <= 0;
+		vif.drv_cb.penable <= 0;
+		vif.drv_cb.pwrite <= 0;
+		vif.drv_cb.paddr <= 0;
+		vif.drv_cb.pwdata <= 0;
+	endtask
+	
+	task drive_apb(apb_seq_item tx);
+		// SETUP phase
+		@(vif.drv_cb);
+		vif.drv_cb.psel <= 1;
+		vif.drv_cb.penable <= 0;
+		vif.drv_cb.pwrite <= tx.pwrite;
+		vif.drv_cb.paddr <= tx.paddr;
+		vif.drv_cb.pwdata <= tx.pwrite ? tx.pwdata : 0;
+
+		// ACCESS phase
+		@(vif.drv_cb);
+		vif.drv_cb.penable <=1;
+		while(!vif.drv_cb.pready) @(vif.drv_cb);
+		if(!tx.pwrite)begin
+			tx.prdata = vif.drv_cb.prdata;
+			tx.pready = vif.drv_cb.pready;
+		end
+
+		//IDLE phase
+		@(vif.drv_cb);
+		vif.drv_cb.psel <= 0;
+		vif.drv_cb.penable <= 0;
+
+		`uvm_info(get_type_name(), $sformatf("complete drv apb: %s",
+						 tx.convert2string()),UVM_MEDIUM);
+	endtask
+endclass
+
+`endif
---
+++ APB_RAM/Code/apb_ram_interface.sv
@@ -0,0 +1,37 @@
+interface apb_if (
+    input logic pclk,
+    input logic presetn
+);
+    logic [7:0] paddr;
+    logic pwrite;
+    logic penable;
+    logic [31:0] pwdata;
+    logic psel;
+    logic [31:0] prdata;
+    logic pready;
+
+    clocking drv_cb @(posedge pclk);
+        default input #1step output #0;
+        output paddr;
+        output pwrite;
+        output penable;
+        output pwdata;
+        output psel;
+        input prdata;
+        input pready;
+    endclocking
+
+    clocking mon_cb @(posedge pclk);
+        default input #1step;
+        input paddr;
+        input pwrite;
+        input penable;
+        input pwdata;
+        input psel;
+        input prdata;
+        input pready;
+    endclocking
+
+    modport mod_drv(clocking drv_cb, input pclk, input presetn);
+    modport mod_mon(clocking drv_cb, input pclk, input presetn);
+endinterface
